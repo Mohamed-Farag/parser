2dfe6d1e1aa71700043a6ad56ac5e33c0b407cca
Mohamed-Farag <33036643+Mohamed-Farag@users.noreply.github.com>
2018-12-17 22:46:46 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,31 +1,138 @@
+from xml.etree import ElementTree as ET
 
+token = "start"    # global variable
 
-# important notes
-  #1) make txt file in E directory naming it (input to parser)
-
-
-
-
-token = ""   # dah global variable
 
 def Error():            # al function dy al mfrood tw2f al brnamg w ttl3 error message
     print("Error")
 
 
+def read-stmt():
+    match("read")
+    match("identifier")
+
+def if-stmt():
+    match("if")
+    exp()
+    match("then")
+    stmt_seq()
+    if (token == "end"):
+        match("end")
+
+    else if (token == "else"):
+        match("else")
+        stmt_seq()
+        match("end")
+
+
+def term():
+    if (token == "term"):
+        term()
+        mulop()
+        factor()
+
+    else if (token == "factor"):
+        factor()
+
+
+def mulop():
+    if (token == "*"):
+        match("*")
+
+    else if (token == "/"):
+        match("/")
+
+
+def factor():
+    if (token == "("):
+        match("(")
+        exp()
+        match(")")
+
+    else if (token == "number")
+    match("number")
+
+    else if (token == "identifier")
+
+
+def program():
+    stmt_seq()
+
+def stmt_seq():
+    statment()
+
+#    while(token == "if"  or token == "repeat" or token == "assign" )        # lsa hn3dlha
+
+
+def statment():
+    if(token == "if"):
+        IF()
+    elif (token == "repeat"):
+        repeat()
+    elif (token == "assign"):
+        assgin()
+    elif (token == "read"):
+        read()
+    elif (token == "write"):
+        write()
+    else:
+        Error()
+
 def read():
     print("read")
 def IF():
     print("if")
-def exp():              # al function dy al mfrood thandle al exp
-    print("exp")
+
+def exp():
+    simple_exp()
+    while(token == "<" or token == ">" or token == "="):
+        comparison_exp()
+        simple_exp()
+
+def comparison_exp():
+    if (token == "<"):
+        match("<")
+    elif (token == ">"):
+        match(">")
+    elif (token == "="):
+        match("=")
+    else:
+        Error()
+
+def assign_stmt():
+    is_identifier()
+    match(":=")
+    exp()
+
+
+def simple_exp():
+    term()
+    while(token == "+" or token == "-"):
+        addop()
+        term()
+
+def addop():
+    if(token == "+"):
+        match("+")
+    elif (token == "-"):
+        match("-")
+
+
+
 def repeat():
-    print("repeat")
+    match("repeat")
+    stmt_seq()
+    match("until")
+    exp()
+
+
 def write():
     print("write")
 
 
 def match(t):
-    if t == token:
+    global token
+    if (token == t):
         token = next_token()
     else:
         Error()
@@ -46,15 +153,12 @@ def next_token():        # this function is used to get the next token in the in
 
 
 if __name__ == '__main__':
+
+
     f = open("E:\input to parser.txt", "r")
-    token=next_token()
-    print(next_token())
-    print(next_token())
-    #read();
-    #IF();
-    #exp();
-    #repeat();
-    #write();
+    token = next_token()
+    print(token)
+    program()
 
 
 
