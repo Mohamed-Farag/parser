2d187f5d481b2d6a2c672c4a5b43f156b5eea2ca
Mohamed-Farag <33036643+Mohamed-Farag@users.noreply.github.com>
2018-12-18 20:40:16 +0200

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,20 +3,33 @@ from xml.etree import ElementTree as ET
 # important notes
   #1) make txt file in E directory naming it (input to parser)
 
-
-
-
 token1 = "start"    # dah global variable
 token2 = "start"    # dah global variable
 
 
-def Error():            # al function dy al mfrood tw2f al brnamg w ttl3 error message
-    print("Syntax Error")
+def program():
+    stmt_seq()
+
+def stmt_seq():
+    statment()
+
+    while(token1 == ";"):       
+        match(";")
+        statment()
 
 
-def read_stmt():
-    match("read")       # token1
-    match("identifier") # token2
+def statment():
+    if(token1 == "if"):
+        if_stmt()
+    elif (token1 == "repeat"):
+        repeat()
+    elif (token1 == "read"):
+        read_stmt()
+    elif (token1 == "write"):
+        write()
+    else:
+        assign_stmt()
+
 
 def if_stmt():
     global token1
@@ -33,76 +46,37 @@ def if_stmt():
         match("end")
 
 
-def term():
-    if (token1 == "term"):
-        term()
-        mulop()
-        factor()
-
-    elif (token1 == "factor"):
-        factor()
-
-
-def mulop():
-    if (token1 == "*"):
-        match("*")
-
-    elif (token1 == "/"):
-        match("/")
-
-
-def factor():
-    global token1
-    if (token1 == "("):
-        match("(")
-        exp()
-        match(")")
-
-    elif (token2 == "Number"):
-        match("Number")
-
-    elif (token2 == "identifier"):
-        match("identifier")
-
-
-def program():
+def repeat():
+    match("repeat")
     stmt_seq()
-
-def stmt_seq():
-    statment()
-
-    while(token2 == "identifier\n" or token1 == "if"  or token1 == "repeat" or token1 == ":=" or token1 == "read" or token1 == "write"):        # lsa hn3dlha
-        statment()
+    match("until")
+    exp()
 
 
-def statment():
-    if(token1 == "if"):
-        IF()
-    elif (token1 == "repeat"):
-        repeat()
-    elif (token1 == "read"):
-        read()
-    elif (token1 == "write"):
-        write()
-    elif (token2 == "identifier\n"):
-        match("identifier")
-    elif (token1 == ":="):
-        match(":=")
-    else:
-        Error()
+def assign_stmt():
+    match("identifier")
+    match(":=")
+    exp()
 
-def read():
-    print("read")
-def IF():
-    print("if")
+
+
+def read_stmt():
+    match("read")
+    match("identifier")
+
+
+def write():
+    match("write")
+    exp()
 
 def exp():
     simple_exp()
     while(token1 == "<" or token1 == ">" or token1 == "="):
-        comparison_exp()
+        comparison_op()
         simple_exp()
 
-def comparison_exp():
+
+def comparison_op():
     if (token1 == "<"):
         match("<")
     elif (token1 == ">"):
@@ -111,11 +85,6 @@ def comparison_exp():
         match("=")
     else:
         Error()
-
-def assign_stmt():
-    match("identifier")
-    match(":=")
-    exp()
 
 
 def simple_exp():
@@ -131,22 +100,45 @@ def addop():
         match("-")
 
 
+def term():
+    factor()
+    while (token1 == "*" or token1 == "/"):
+        mulop()
+        factor()
 
-def repeat():
-    match("repeat")
-    stmt_seq()
-    match("until")
-    exp()
+
+def mulop():
+    if (token1 == "*"):
+        match("*")
+
+    elif (token1 == "/"):
+        match("/")
 
 
-def write():
-    print("write")
+def factor():
+    global token1
+    global token2
+    if (token1 == "("):
+        match("(")
+        exp()
+        match(")")
+
+    elif (token2 == "Number"):
+        match("Number")
+
+    elif (token2 == "identifier"):
+        match("identifier")
+
+
+
+
+
 
 
 def match(t):
     global token1
     global token2
-    if (token1 == t or token2 == t+"\n"):
+    if (token1 == t or token2 == t):
         token1,token2 = next_token()
     else:
         Error()
@@ -163,27 +155,18 @@ def is_number(t):
 def next_token():        # this function is used to get the next token in the input to parser file
     t = f.readline()
     index = t.find(",")
-    return t[:index],t[index+1:]
+    t1 = t[:index]
+    t2 = t[index+1:].strip()
+    return t1,t2
 
 
-
-
-
-
+def Error():  # al function dy al mfrood tw2f al brnamg w ttl3 error message
+    print("Syntax Error")
 
 
 if __name__ == '__main__':
-
-
 
     f = open("E:\input to parser.txt", "r")
     token1,token2 = next_token()
     #print(token1,token2)
     program()
-
-
-
-
-
-
-
